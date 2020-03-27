3b270999dad84e1b500edf21093f85d8d3b1badb
Mike Goren <mike@mortalwombat.net>
2020-03-27 16:22:22 -0700

    add track

--- config/initializers/tracks.rb
+++ config/initializers/tracks.rb
@@ -7,6 +7,7 @@ CURRENT_TRACKS = [
   '2020-05-26 to 2020-07-30 WEB Part-Time Intro to Programming',
   '2020-05-26 to 2020-10-11 PDX Part-Time JS/React',
   '2020-05-26 to 2020-10-11 SEA Part-Time JS/React',
+  '2020-05-26 to 2020-10-11 WEB Part-Time JS/React',
   '2020-08-03 to 2021-02-19 PDX Ruby/React',
   '2020-08-03 to 2021-02-19 SEA C#/React',
   '2020-08-04 to 2020-10-08 PDX Part-Time Intro to Programming',
