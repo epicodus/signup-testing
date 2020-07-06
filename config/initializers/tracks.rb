4de9bf97c0a4c9308ed4e1bf24297baa68bbc508
Mike Goren <mike@mortalwombat.net>
2020-07-06 14:01:54 -0700

    update tracks

--- config/initializers/tracks.rb
+++ config/initializers/tracks.rb
@@ -6,16 +6,16 @@ CURRENT_TRACKS = [
   '2020-08-04 to 2020-10-01 PDX Part-Time Intro to Programming',
   '2020-08-04 to 2020-10-01 SEA Part-Time Intro to Programming',
   '2020-08-04 to 2020-10-01 WEB Part-Time Intro to Programming',
-  '2020-08-04 to 2021-01-24 PDX Part-Time JS/React',
-  '2020-08-04 to 2021-01-24 SEA Part-Time JS/React',
-  '2020-08-04 to 2021-01-24 WEB Part-Time JS/React',
+  '2020-08-04 to 2020-12-27 PDX Part-Time JS/React',
+  '2020-08-04 to 2020-12-27 SEA Part-Time JS/React',
+  '2020-08-04 to 2020-12-27 WEB Part-Time JS/React',
   '2020-10-12 to 2021-04-30 PDX C#/React',
   '2020-10-12 to 2021-04-30 SEA C#/React',
   '2020-10-12 to 2021-04-30 WEB C#/React',
   '2020-10-13 to 2020-12-17 PDX Part-Time Intro to Programming',
   '2020-10-13 to 2020-12-17 SEA Part-Time Intro to Programming',
   '2020-10-13 to 2020-12-17 WEB Part-Time Intro to Programming',
-  '2020-10-13 to 2021-04-04 PDX Part-Time JS/React',
-  '2020-10-13 to 2021-04-04 SEA Part-Time JS/React',
-  '2020-10-13 to 2021-04-04 WEB Part-Time JS/React'
+  '2020-10-13 to 2021-03-14 PDX Part-Time JS/React',
+  '2020-10-13 to 2021-03-14 SEA Part-Time JS/React',
+  '2020-10-13 to 2021-03-14 WEB Part-Time JS/React'
 ]
